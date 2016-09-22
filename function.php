7a466996b26b68bd97f82d2cb9fc4d4eaf1d80fa
caohejie <9568702@qq.com>
2016-09-22 09:17:12 +0800

    2

--- function.php
+++ function.php
@@ -33,3 +33,97 @@ function getIp(){
     
 }
 
+//获取随机数
+function GetRand($len, $type = 2)
+{
+    if ($type == 1) {
+        $chars = array(
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"
+        );
+    }
+
+    if ($type == 2) {
+        $chars = array(
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"
+        );
+    }
+
+    $charsLen = count($chars) - 1;
+    shuffle($chars);
+    $output = "";
+    for ($i = 0; $i < $len; $i ++) {
+        $output .= $chars[mt_rand(0, $charsLen)];
+    }
+    return $output;
+}
+
+
