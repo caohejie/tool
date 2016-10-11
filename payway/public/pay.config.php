0be03a50fe8d66d3a73e79c39ae4e2cd922129f8
caohejie <9568702@qq.com>
2016-10-11 15:25:12 +0800

    2

--- payway/public/pay.config.php
+++ payway/public/pay.config.php
@@ -1,14 +1,14 @@
 <?php
 //微信相关
-define("WXAPPID", 'wxe0a3ef3c86a6ce9b');//绑定支付的APPID（必须配置，开户邮件中可查看）
-define("WXMCHID", '1372748102');//商户号（必须配置，开户邮件中可查看）
-define("WXKEY", 'ec64ba91d4e049eb85a57bb8e2a1e92e');//商户支付密钥，参考开户邮件设置（必须配置，登录商户平台自行设置） 设置地址：https://pay.weixin.qq.com/index.php/account/api_cert
-define("WXAPPSECRET", 'cb8db27fa1cd8d672ede9ce29feefe28');//公众帐号secert（仅JSAPI支付的时候需要配置， 登录公众平台，进入开发者中心可设置）获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
-define("WXHUIDIAO", "http://payway.babidou.net/class/wx/huidiao.php");
+define("WXAPPID", 'XXXXXX');//绑定支付的APPID（必须配置，开户邮件中可查看）
+define("WXMCHID", 'XXXX');//商户号（必须配置，开户邮件中可查看）
+define("WXKEY", 'XXXXXXX');//商户支付密钥，参考开户邮件设置（必须配置，登录商户平台自行设置） 设置地址：https://pay.weixin.qq.com/index.php/account/api_cert
+define("WXAPPSECRET", 'XXXXXXX');//公众帐号secert（仅JSAPI支付的时候需要配置， 登录公众平台，进入开发者中心可设置）获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
+define("WXHUIDIAO", "XXXXXXXX");
 //支付宝
 
-define("partner", "2088221318759442");
-define("seller_id", "2088221318759442");
-define("key", "zovy931hek7np8m0oyyjg5ks2kljvr8x");
-define("notify_url", "http://www.baidu.com");
-define("return_url", "http://www.baidu.com");
+define("partner", "XXXX");
+define("seller_id", "XXXXX");
+define("key", "XXXXX");
+define("notify_url", "XXXXX");
+define("return_url", "XXXXX");
